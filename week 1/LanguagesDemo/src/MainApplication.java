8266f0d500fd8dec914f71398e2957f03bb0e126
Martin Enriquez <martinantraxelm1@gmail.com>
2024-01-29 14:06:52 -0700

    demo code walkthroughs

---
+++ week 1/LanguagesDemo/src/MainApplication.java
@@ -0,0 +1,9 @@
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HTML");
+        System.out.println("CSS");
+        System.out.println("JavaScript");
+
+    }
+}
